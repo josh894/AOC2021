6ffa32d8601b81f649f5db27e0bb1ba335854281
Josh <82674021+josh894@users.noreply.github.com>
2021-12-04 20:44:49 -0500

    Day1+2
    Started late, Day 1 and 2 solutions

---
+++ Day1/Day1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+	int input[10000];
+	int length = 0;
+	int total = 0;
+	int j = 0;
+	int A[3];
+	int B[3];
+	int aStart = 0;
+	int aSum = 0;
+	int bSum = 0;
+
+
+	FILE *fp = fopen("Day1Input.txt", "r");
+	if(fp == NULL){
+		exit(1);
+	}
+
+	while(fscanf(fp, "%d", &input[j]) != EOF){
+		length++;
+		j++;
+	}
+
+	/*for(int i = 1; i < length; i++){
+		if(input[i] > input[i-1]){
+			total++;
+		}
+	}*/
+
+	for(int i = 0; i < length; i++){
+		if(i == 0){
+			A[0] = input[i];
+			A[1] = input[i + 1];
+			A[2] = input[i + 2];
+			aSum = A[0] + A[1] + A[2];
+		}
+		if(i == 1){
+			B[0] = input[i];
+			B[1] = input[i + 1];
+			B[2] = input[i + 2];
+			bSum = B[0] + B[1] + B[2];
+		}
+
+		if(i == aStart + 2){
+			if(bSum > aSum){
+				total++;
+			}
+
+			aStart = i;
+			A[0] = input[i];
+			A[1] = input[i + 1];
+			A[2] = input[i + 2];
+			aSum = A[0] + A[1] + A[2];
+
+			if(aSum > bSum){
+				total++;
+			}
+			
+			B[0] = input[i + 1];
+			B[1] = input[i + 2];
+			B[2] = input[i + 3];
+			bSum = B[0] + B[1] + B[2];
+		}
+
+	}
+	printf("%d\n", total);
+	fclose(fp);
+}
---
+++ Day2/Day2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+	int length = 0;	
+	char direction[20];
+	int hori = 0;
+	int vert = 0;
+	int total = 0;
+	int aim = 0 ;
+
+	FILE *fp = fopen("Day2Input.txt", "r");
+	if(fp == NULL){
+		exit(1);
+	}
+
+	while(fscanf(fp, "%s %d", direction, &length) != EOF){
+		printf("%d\n", length);
+		if(strcmp(direction, "forward") == 0){
+			hori = hori + length;
+			vert = vert + (length * aim);
+			printf("%s\n", direction);
+		}
+		if(strcmp(direction, "down") == 0){
+			aim = aim + length;
+		}
+		if(strcmp(direction, "up") == 0){
+			aim = aim - length;
+		}
+	}
+
+	total = hori * vert;
+	printf("%d\n", total);
+}
